98cc5d567b7541818dc01ae36bcc8df2f6c0de28
张耿坚 <zhanggengjian@99bicycyle.com>
2018-07-17 14:46:15 +0800

    兑吧

--- src/main/java/com/applet/controller/StoreController/StoreController.java
+++ src/main/java/com/applet/controller/StoreController/StoreController.java
@@ -5,6 +5,8 @@ import com.applet.entity.store.AutoLoginReq;
 import com.applet.entity.store.DuibaReq;
 import com.applet.service.StoreService;
 import com.applet.utils.AppletResult;
+import com.applet.utils.ReturnDataUtil;
+import com.applet.utils.common.JSONUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +27,15 @@ public class StoreController {
         String adminId = duibaReq.getAdminId();
         AutoLoginReq req = new AutoLoginReq();
         req.setUserId(adminId);
-        return storeService.duiba(req);
+        AppletResult result = storeService.duiba(req);
+        ReturnDataUtil dataUtil = JSONUtil.parseObject(JSONUtil.toJSONString(result.getData()),ReturnDataUtil.class);
+        String url=dataUtil.getReturnData().toString();
+        //http://www.duiba.com.cn/autoLogin/autologin?uid=15554313215&credits=null&sign=4fbd01848f12b7e974893f11a288b87f&appKey=bHqyPxCrAChd1iDfUdLe1J4gdtJ&timestamp=1531469842586&
+        url = url.substring(24);
+        url = "https://home.m.duiba.com.cn/"+url;
+        ReturnDataUtil returnDataUtil = new ReturnDataUtil();
+        returnDataUtil.setReturnData(url);
+        result.setData(returnDataUtil);
+        return result;
     }
 }
